46ef910376e38197c5fba31ea6dd82d5f7f8b0c1
Rodrigo Soares de Paula <rsoarespaula@gmail.com>
2016-05-01 06:10:36 -0300

    Melhor uso da OO

---
+++ nerdcast.rb
@@ -0,0 +1,10 @@
+class Nerdcast
+
+  attr_accessor :title
+  attr_accessor :mp3_file
+  attr_accessor :dest_file_name
+  attr_accessor :dest_file
+  attr_accessor :size
+
+
+end
--- nerdcast_downloader.rb
+++ nerdcast_downloader.rb
@@ -1,52 +1,93 @@
 require 'nokogiri'
 require 'open-uri'
 require 'open_uri_redirections'
+require 'filesize'
+require_relative 'nerdcast'
 
 class NerdcastDownloader
 
 	def download 
-		file = File.new('feed.xml')
+    if !Dir.exists?("episodes")
+      Dir.mkdir("episodes")
+    end
 
-		doc = Nokogiri::XML(file)
+    episodes = get_episodes
+    
+    episodes_to_download = select_episodes_to_download(episodes)
 
-		doc.xpath('//channel/item').each do |item|
-			title = item.at_xpath('title').content
+    episodes_to_download.each do |nc|
+      download_episode(nc)
+    end
 
-			mp3_file = item.at_xpath('enclosure').attr('url')
+    puts 
+	end
+
+  def select_episodes_to_download(episodes)
+    episodes_to_download = Array.new
+
+    total_size = 0
+
+    episodes.each do |nc|
+      if !File.size?(nc.dest_file)
+        episodes_to_download.push(nc)
+        total_size += nc.size
+      else
+        puts "Episodio #{nc.title} ja existe. Ignorando..."
+      end
+    end
+
+    total_size_pretty = Filesize.from(total_size.to_s + " B").pretty
+    puts "Total a ser baixado: #{total_size_pretty}"
+    puts
+
+    episodes_to_download
+  end
+
+  def get_episodes
+    file = File.new('feed.xml')
+
+    doc = Nokogiri::XML(file)
+    
+    episodes_array = Array.new
+
+    doc.xpath('//channel/item').each do |item|
+
+      title = item.at_xpath('title').content
 
       dest_file = title.dup + ".mp3"
 
       #file_name_destination = title.dup
-			dest_file = sanitize_filename(dest_file)
+      dest_file_name = sanitize_filename(dest_file)
 
-      puts "Episodio          : #{title}"
-      puts "Arquivo Destino   : #{dest_file}"
-      puts "Arquivo Original  : #{mp3_file}"
-      
-      download_file(mp3_file, dest_file)
-      puts 
+      dest_file = "episodes/" + dest_file_name
 
-		end
-	end
+      size = item.at_xpath('enclosure').attr('length').to_i
 
-  def download_file(file, destination_name)
-    if !Dir.exists?("episodes")
-      Dir.mkdir("episodes")
+      nc = Nerdcast.new
+      nc.title = title
+      nc.mp3_file = item.at_xpath('enclosure').attr('url')
+      nc.dest_file = dest_file
+      nc.dest_file_name = dest_file_name
+      nc.size = size
+      episodes_array.push(nc)
     end
-    destination_name = "episodes/" + destination_name
 
-    file_exists = File.size?(destination_name)
-    if file_exists
-      puts "Arquivo ja existe, ignorando..."
-    else
-      puts "Baixando..."
+    episodes_array
+  end
 
-      File.open(destination_name, "wb") do |saved_file|
-        open(file, "rb", :allow_redirections => :all) do |read_file|
-          saved_file.write(read_file.read)
-        end
+  def download_episode(nc)
+    
+    puts "Episodio          : #{nc.title}"
+    puts "Arquivo Destino   : #{nc.dest_file}"
+    puts "Arquivo Original  : #{nc.mp3_file}"  
+
+    File.open(nc.dest_file, "wb") do |saved_file|
+      open(nc.mp3_file, "rb", :allow_redirections => :all) do |read_file|
+        saved_file.write(read_file.read)
       end
     end
+
+    puts
   end
 
 
